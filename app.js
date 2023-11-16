3ae002d9d56f9bf7b4d0d0622ee0c5fabc2429ab
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-11-15 20:39:06 -0800

    progress

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var app = express();
-var port = 3000;
+// Code From socket.io Docs
+// https://socket.io/docs/v4/
 
-app.use(express.static(__dirname + '/'));
-app.listen(port);
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 
-console.log("Listening on:");
-console.log(`http://localhost:${port}`);
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.use(express.static(__dirname + "/"));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
