4861d2fd3d9cc52fb89f9d830a42e44ab2cfc0f7
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-12-12 11:54:41 -0800

    init nextjs app

--- back-end/getDistance.js
+++
@@ -1,9 +0,0 @@
-function getDistance(point1X, point1Y, point2X, point2Y) {
-    const xDistance = point1X - point2X;
-    const yDistance = point1Y - point2Y;
-    // Pythagorean Theorem
-    const distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-    return distance;
-}
-
-module.exports = { getDistance };
